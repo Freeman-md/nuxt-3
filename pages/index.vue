b3247e0371b08f6cda75c79bb482452818786cb5
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 11:40:03 +0000

    implement multiple useAsyncData calls with caching in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp()
+const KEY = 'test';
+const { data: data } = await useAsyncData(KEY, async () => {
+  console.log('test');
+  return {};
+}, {
+  getCachedData(key) {
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
+const { data: data2 } = await useAsyncData(KEY, async () => {
+  console.log('test');
+  return {};
+},  {
+  getCachedData(key) {
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
+const { data: data3 } = await useAsyncData(KEY, async () => {
+  console.log('test');
+  return {};
+},  {
+  getCachedData(key) {
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
+
+</script>
 
 <template>
   <div>
