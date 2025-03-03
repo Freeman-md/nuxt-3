7c824206d54c9f81f525915371ca3fb2a5bf155e
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 10:57:46 +0000

    implement movie search functionality in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup>
+const query = ref("")
+const movies = ref([])
+
+async function search() {
+  const { Search } = await $fetch(`https://www.omdbapi.com/?apikey=c636c52f&s=${query.value}`)
+
+  movies.value = Search
+}
+
+</script>
 
 <template>
-  <div>
-    Page: Home Page
-  </div>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query">
+    <button>Search</button>
+  </form>
+
+  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+    <li v-for="movie in movies" :key="movie.imdbID">
+      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+        <img :src="movie.Poster" :alt="movie.title" width="100" />
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
