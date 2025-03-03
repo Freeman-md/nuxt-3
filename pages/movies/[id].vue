324efbc554e6a43bcf4e233158aac92cff4f3785
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 11:18:40 +0000

    update movie search query to default to "batman" and modify async data fetching in movie detail page

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,7 +1,7 @@
 <script setup>
 const route = useRoute()
 
-const { data } = useAsyncData(() => {
+const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`https://www.omdbapi.com/?apikey=c636c52f&i=${route.params.id}`)
 }) 
 </script>
