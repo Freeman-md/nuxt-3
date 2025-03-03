768e82695966d6d255a7b93c5c318c9e2181f95c
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 20:53:04 +0000

    remove redundant useAsyncData calls in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,4 @@
 <script setup lang="ts">
-const nuxtApp = useNuxtApp()
-const KEY = 'test';
-const { data: data } = await useAsyncData(KEY, async () => {
-  console.log('test');
-  return {};
-}, {
-  getCachedData(key) {
-    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-  },
-});
-const { data: data2 } = await useAsyncData(KEY, async () => {
-  console.log('test');
-  return {};
-},  {
-  getCachedData(key) {
-    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-  },
-});
-const { data: data3 } = await useAsyncData(KEY, async () => {
-  console.log('test');
-  return {};
-},  {
-  getCachedData(key) {
-    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-  },
-});
 
 </script>
 
