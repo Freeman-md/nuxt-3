ea0d8d8f03d8c8e99f1da62c4449d4dad8aa6637
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 11:36:01 +0000

    handle error for incorrect IMDb ID in movie detail page

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,10 +1,21 @@
 <script setup>
 const route = useRoute()
 
-const { data } = useFetch(`https://www.omdbapi.com/?apikey=c636c52f&i=${route.params.id}`, {
-  pick: ["Plot", "Title"],
-  key: `/movies/${route.params.id}`
+const { data, error } = await useFetch(`https://www.omdbapi.com/?apikey=c636c52f&i=${route.params.id}`, {
+  pick: ["Plot", "Title", "Error"],
+  key: `/movies/${route.params.id}`,
 }) 
+
+if (error.value) {
+  // handle accordingly
+}
+
+if (data.value.Error == "Incorrect IMDb ID.") {
+      showError({
+        statusCode: 404,
+        statusMessage: "Page Not Found"
+      })
+    }
 </script>
 
 <template>
