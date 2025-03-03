cb8d4fbc7ca196b9d82492c21a6dfc772e124670
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 11:08:38 +0000

    refactor movie search functionality into a separate component and update index pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,9 @@
-<script setup>
-const query = ref("")
-const movies = ref([])
-
-async function search() {
-  const { Search } = await $fetch(`https://www.omdbapi.com/?apikey=c636c52f&s=${query.value}`)
-
-  movies.value = Search
-}
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <form @submit.prevent="search">
-    <input type="text" v-model="query">
-    <button>Search</button>
-  </form>
-
-  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
-    <li v-for="movie in movies" :key="movie.imdbID">
-      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
-        <img :src="movie.Poster" :alt="movie.title" width="100" />
-      </NuxtLink>
-    </li>
-  </ul>
+  <div>
+    Page: Index
+  </div>
 </template>
 
 <style scoped></style>
