04948a40f7694a54b0520e4c9726783da7adeefd
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-03 21:04:12 +0000

    add dynamic page metadata and improve error handling for movie details

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -2,20 +2,26 @@
 const route = useRoute()
 
 const { data, error } = await useFetch(`https://www.omdbapi.com/?apikey=c636c52f&i=${route.params.id}`, {
-  pick: ["Plot", "Title", "Error"],
+  pick: ["Plot", "Title", "Error", "Poster"],
   key: `/movies/${route.params.id}`,
-}) 
-
-if (error.value) {
-  // handle accordingly
-}
+})
 
 if (data.value.Error == "Incorrect IMDb ID.") {
-      showError({
-        statusCode: 404,
-        statusMessage: "Page Not Found"
-      })
-    }
+  showError({
+    statusCode: 404,
+    statusMessage: "Page Not Found"
+  })
+}
+
+useHead({
+  title: data.value.Title,
+  meta: [
+    { name: "description", content: data.value.Plot },
+    { property: "og:description", content: data.value.Plot },
+    { property: "og:image", content: data.value.Poster },
+    { name: "twitter:card", content: `summary_large_image` },
+  ]
+})
 </script>
 
 <template>
