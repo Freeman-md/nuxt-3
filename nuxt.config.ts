c3cec5888d21a031e6144106803febc9a23e8ab4
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-12 11:09:31 +0000

    add @pinia/nuxt module for state management integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: [
+    [
+      "@pinia/nuxt", {
+        autoImports: ["defineStore", "acceptHMRUpdate"]
+      }
+    ]
+  ],
+  // alias: {
+  //   pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  // }
 })
