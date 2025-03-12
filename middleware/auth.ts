5b23638dff99b55f3e3c3112de5cc30cd9c286a9
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-12 10:39:39 +0000

    redirect unauthenticated users to login page when accessing admin route

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const userIsLoggedIn = false;
     if (!userIsLoggedIn) {
-        return abortNavigation("You're not allowed to visit /admin");
+        return navigateTo({
+            name: "login"
+        })
     }
 })
