d114d2f66e80a1b8ae51c4d70f9050e0c52e2d1c
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-03-12 11:35:33 +0000

    remove imports configuration for store directories in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,10 +9,4 @@ export default defineNuxtConfig({
       }
     ]
   ],
-  imports: {
-    dirs: ["stores"]
-  },
-  // alias: {
-  //   pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
-  // }
 })
